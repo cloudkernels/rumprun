cde56d36e9659df0354bc4bef3e2f99f54ed0233
Charalampos Mainas <cmainas@nubificus.co.uk>
2024-08-29 14:24:38 +0100

    Allow gateway address to be in different subnet than IP
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- platform/solo5/librumpnet_ukvmif/ukvmif_user.c
+++ platform/solo5/librumpnet_ukvmif/ukvmif_user.c
@@ -141,6 +141,10 @@ VIFHYPER_CREATE(const char *devstr, struct virtif_sc *vif_sc, uint8_t *enaddr,
 		solo5_exit(SOLO5_EXIT_FAILURE);
 	}
 
+	/* initialize the flag -- some compilers add garbage there */
+	if (viu->viu_dying != 0) {
+		viu->viu_dying = 0;
+	}
 	viu->viu_virtifsc = vif_sc;
 
 	viu->viu_rcvthr = bmk_sched_create("ukvmifp",
